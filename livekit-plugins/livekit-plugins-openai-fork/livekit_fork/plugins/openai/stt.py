7d1fb03b93cbad9249d0ccd74832af2c388d3054
Sean Muirhead <sean@assorthealth.com>
2024-08-02 09:09:48 -0700

    move openai plugin

---
+++ livekit-plugins/livekit-plugins-openai-fork/livekit_fork/plugins/openai/stt.py
@@ -0,0 +1,120 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import dataclasses
+import io
+import os
+import wave
+from dataclasses import dataclass
+
+import aiohttp
+from livekit_fork import agents
+from livekit_fork.agents import stt, utils
+from livekit_fork.agents.utils import AudioBuffer
+
+from .models import WhisperModels
+from .utils import get_base_url
+
+
+@dataclass
+class _STTOptions:
+    language: str
+    detect_language: bool
+    model: WhisperModels
+    api_key: str
+    endpoint: str
+
+
+class STT(stt.STT):
+    def __init__(
+        self,
+        *,
+        language: str = "en",
+        detect_language: bool = False,
+        model: WhisperModels = "whisper-1",
+        api_key: str | None = None,
+        base_url: str | None = None,
+        http_session: aiohttp.ClientSession | None = None,
+    ):
+        super().__init__(
+            capabilities=stt.STTCapabilities(streaming=False, interim_results=False)
+        )
+        api_key = api_key or os.environ.get("OPENAI_API_KEY")
+        if not api_key:
+            raise ValueError("OPENAI_API_KEY must be set")
+
+        if detect_language:
+            language = ""
+
+        self._opts = _STTOptions(
+            language=language,
+            detect_language=detect_language,
+            model=model,
+            api_key=api_key,
+            endpoint=os.path.join(get_base_url(base_url), "audio/transcriptions"),
+        )
+        self._session = http_session
+
+    def _ensure_session(self) -> aiohttp.ClientSession:
+        if not self._session:
+            self._session = utils.http_context.http_session()
+
+        return self._session
+
+    def _sanitize_options(self, *, language: str | None = None) -> _STTOptions:
+        config = dataclasses.replace(self._opts)
+        config.language = language or config.language
+        return config
+
+    async def recognize(
+        self, buffer: AudioBuffer, *, language: str | None = None
+    ) -> stt.SpeechEvent:
+        config = self._sanitize_options(language=language)
+
+        buffer = agents.utils.merge_frames(buffer)
+        io_buffer = io.BytesIO()
+        with wave.open(io_buffer, "wb") as wav:
+            wav.setnchannels(buffer.num_channels)
+            wav.setsampwidth(2)  # 16-bit
+            wav.setframerate(buffer.sample_rate)
+            wav.writeframes(buffer.data)
+
+        form = aiohttp.FormData()
+        form.add_field("file", io_buffer.getvalue(), filename="my_file.wav")
+        form.add_field("model", config.model)
+
+        if config.language:
+            form.add_field("language", config.language)
+
+        form.add_field("response_format", "json")
+
+        async with self._ensure_session().post(
+            self._opts.endpoint,
+            headers={"Authorization": f"Bearer {config.api_key}"},
+            data=form,
+        ) as resp:
+            data = await resp.json()
+            if "text" not in data or "error" in data:
+                raise ValueError(f"Unexpected response: {data}")
+
+            return _transcription_to_speech_event(data, config.language)
+
+
+def _transcription_to_speech_event(transcription: dict, language) -> stt.SpeechEvent:
+    return stt.SpeechEvent(
+        type=stt.SpeechEventType.FINAL_TRANSCRIPT,
+        alternatives=[stt.SpeechData(text=transcription["text"], language=language)],
+    )
---
+++ livekit-plugins/livekit-plugins-openai-fork/livekit_fork/plugins/openai/tts.py
@@ -0,0 +1,155 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from typing import AsyncContextManager
+
+import httpx
+from livekit_fork.agents import tts, utils
+
+import openai
+
+from .log import logger
+from .models import TTSModels, TTSVoices
+from .utils import AsyncAzureADTokenProvider, get_base_url
+
+OPENAI_TTS_SAMPLE_RATE = 24000
+OPENAI_TTS_CHANNELS = 1
+
+
+@dataclass
+class _TTSOptions:
+    model: TTSModels
+    voice: TTSVoices
+    endpoint: str
+    speed: float
+
+
+class TTS(tts.TTS):
+    def __init__(
+        self,
+        *,
+        model: TTSModels = "tts-1",
+        voice: TTSVoices = "alloy",
+        speed: float = 1.0,
+        base_url: str | None = None,
+        client: openai.AsyncClient | None = None,
+    ) -> None:
+        super().__init__(
+            capabilities=tts.TTSCapabilities(
+                streaming=False,
+            ),
+            sample_rate=OPENAI_TTS_SAMPLE_RATE,
+            num_channels=OPENAI_TTS_CHANNELS,
+        )
+
+        self._client = client or openai.AsyncClient(
+            base_url=get_base_url(base_url),
+            http_client=httpx.AsyncClient(
+                timeout=5.0,
+                follow_redirects=True,
+                limits=httpx.Limits(
+                    max_connections=1000,
+                    max_keepalive_connections=100,
+                    keepalive_expiry=120,
+                ),
+            ),
+        )
+
+        self._opts = _TTSOptions(
+            model=model,
+            voice=voice,
+            endpoint=os.path.join(get_base_url(base_url), "audio/speech"),
+            speed=speed,
+        )
+
+    @staticmethod
+    def create_azure_client(
+        *,
+        model: TTSModels = "tts-1",
+        voice: TTSVoices = "alloy",
+        speed: float = 1.0,
+        azure_endpoint: str | None = None,
+        azure_deployment: str | None = None,
+        api_version: str | None = None,
+        api_key: str | None = None,
+        azure_ad_token: str | None = None,
+        azure_ad_token_provider: AsyncAzureADTokenProvider | None = None,
+        organization: str | None = None,
+        project: str | None = None,
+        base_url: str | None = None,
+    ) -> TTS:
+        """
+        This automatically infers the following arguments from their corresponding environment variables if they are not provided:
+        - `api_key` from `AZURE_OPENAI_API_KEY`
+        - `organization` from `OPENAI_ORG_ID`
+        - `project` from `OPENAI_PROJECT_ID`
+        - `azure_ad_token` from `AZURE_OPENAI_AD_TOKEN`
+        - `api_version` from `OPENAI_API_VERSION`
+        - `azure_endpoint` from `AZURE_OPENAI_ENDPOINT`
+        """
+
+        azure_client = openai.AsyncAzureOpenAI(
+            azure_endpoint=azure_endpoint,
+            azure_deployment=azure_deployment,
+            api_version=api_version,
+            api_key=api_key,
+            azure_ad_token=azure_ad_token,
+            azure_ad_token_provider=azure_ad_token_provider,
+            organization=organization,
+            project=project,
+            base_url=base_url,
+        )  # type: ignore
+
+        return TTS(model=model, voice=voice, speed=speed, client=azure_client)
+
+    def synthesize(self, text: str) -> "ChunkedStream":
+        stream = self._client.audio.speech.with_streaming_response.create(
+            input=text,
+            model=self._opts.model,
+            voice=self._opts.voice,
+            response_format="mp3",
+            speed=self._opts.speed,
+        )
+
+        return ChunkedStream(stream, text, self._opts)
+
+
+class ChunkedStream(tts.ChunkedStream):
+    def __init__(
+        self,
+        oai_stream: AsyncContextManager[openai.AsyncAPIResponse[bytes]],
+        text: str,
+        opts: _TTSOptions,
+    ) -> None:
+        super().__init__()
+        self._opts, self._text = opts, text
+        self._oai_stream = oai_stream
+
+    @utils.log_exceptions(logger=logger)
+    async def _main_task(self):
+        request_id = utils.shortuuid()
+        segment_id = utils.shortuuid()
+        decoder = utils.codecs.Mp3StreamDecoder()
+        async with self._oai_stream as stream:
+            async for data in stream.iter_bytes(4096):
+                for frame in decoder.decode_chunk(data):
+                    self._event_ch.send_nowait(
+                        tts.SynthesizedAudio(
+                            request_id=request_id, segment_id=segment_id, frame=frame
+                        )
+                    )
